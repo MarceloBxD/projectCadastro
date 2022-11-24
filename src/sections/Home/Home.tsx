9cad13fa55926b89b506e3f2931282de0dd6cdf8
Marcelo Bracet <92768231+MarceloBxD@users.noreply.github.com>
2022-11-24 19:07:01 -0300

    showing password

--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -6,29 +6,28 @@ import {
   FormControl,
   Button,
   FormHelperText,
+  Img
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../firebaseConfig/firebase";
+import passwordIcon from "../../assets/images/eye.webp";
 
 export const Home = () => {
   const [isfilled, setIsFilled] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const [
-    createUserWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useCreateUserWithEmailAndPassword(auth)
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
 
   const handleSignIn = (e: any) => {
     e.preventDefault();
 
-    createUserWithEmailAndPassword(email, password)
-  }
+    createUserWithEmailAndPassword(email, password);
+  };
 
   const analyzingIsFilled = () => {
     if (email && password) {
@@ -47,7 +46,7 @@ export const Home = () => {
       <Flex
         gap="10px"
         flexDir="column"
-        justify='center'
+        justify="center"
         align="center"
         w="350px"
         bgColor="#161712"
@@ -60,50 +59,54 @@ export const Home = () => {
         <Text color="#fff" fontWeight="500" fontSize="22px">
           Login
         </Text>
-        <FormControl gap='10px' padding='20px'>
-        <form style={{display: 'flex', flexDirection: 'column', gap: '10px'}} onSubmit={handleSignIn}>
-          <Input
-            type="email"
-            color="#fff"
-            size="sm"
-            variant="flushed"
-            w="300px"
-            value={email}
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            color="#fff"
-            size="sm"
-            variant="flushed"
-            w="300px"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Senha"
-          />
-          <FormHelperText>
-            Insira dados condizentes com o que está sendo pedido.
-          </FormHelperText>
-          <Button
-            type='submit'
-            onClick={() => setIsLoading(true)}
-            isLoading={isLoading === true ? true : false}
-            loadingText="Carregando"
-            color="#595FD9"
-            borderColor="#595FD9"
-            variant="ghost"
-            spinnerPlacement="start"
-            size="sm"
-            disabled={isfilled === true ? false : true}
+        <FormControl gap="10px" padding="20px">
+          <form
+            style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+            onSubmit={handleSignIn}
           >
-            Logar
-          </Button>
+            <Input
+              type="email"
+              color="#fff"
+              size="sm"
+              variant="flushed"
+              w="300px"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Flex>
+              <Input
+                type={showPassword === true ? "text" : "password"}
+                color="#fff"
+                size="sm"
+                variant="flushed"
+                w="300px"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Senha"
+              />
+              <Img cursor='pointer' onClick={() => setShowPassword(!showPassword)} w='25px' objectFit='cover' src={passwordIcon}/>
+            </Flex>
+            <FormHelperText>
+              Insira dados condizentes com o que está sendo pedido.
+            </FormHelperText>
+            <Button
+              type="submit"
+              onClick={() => setIsLoading(true)}
+              isLoading={isLoading === true ? true : false}
+              loadingText="Carregando"
+              color="#595FD9"
+              borderColor="#595FD9"
+              variant="ghost"
+              spinnerPlacement="start"
+              size="sm"
+              disabled={isfilled === true ? false : true}
+            >
+              Logar
+            </Button>
           </form>
         </FormControl>
-        <Text color="#fff">
-          Ainda não possui uma conta?
-        </Text>
+        <Text color="#fff">Ainda não possui uma conta?</Text>
         <Text color="#F3E8EE" textDecoration="underline">
           <Link to="/cadastro">cadastre-se</Link>
         </Text>
